25f3cdef9bfd9e4ee7e361a96760e24781587f9b
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-08-11 00:37:23 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 require_once(__DIR__ . '/vendor/autoload.php');
 
 // Configure API key authorization: Apikey
-$config = Swagger\Client\Configuration::getDefaultConfiguration()->setApiKey('Apikey', 'YOUR_API_KEY');
+$config = Swagger\Client\Configuration::getDefaultConfiguration()->setApiKey('Apikey', 'APIKEY-HERE');
 
 
 
 $apiInstance = new Swagger\Client\Api\GenerateBarcodeApi(
     
     
-    new GuzzleHttp\Client(),
+    new GuzzleHttp\Client(array('verify'          => false)),
     $config
 );
 $value = "value_example"; // string | QR code text to convert into the QR code barcode
